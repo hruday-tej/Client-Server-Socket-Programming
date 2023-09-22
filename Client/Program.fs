5319b6d3a640072e91fbf4a8673aaf134902c078
Hruday Tej Akkaladevi <hruday.t.akkaladevi@tsgforce.com>
2023-09-21 20:36:34 -0400

    Modular Code with Multi Client Connect

---
+++ Client/Program.fs
@@ -0,0 +1,26 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+open System.Net
+open System.Net.Sockets
+
+module ClientSideProgram=
+    let ipAddress = IPAddress.Parse("127.0.0.1")
+
+    let connect (server : string, message : string ) = 
+        try
+            let port = 13000;
+            let tcpClient: TcpClient = TcpClient("127.0.0.1", port)
+            let data : byte[] = System.Text.Encoding.ASCII.GetBytes(message)
+            let stream = tcpClient.GetStream()
+            stream.Write(data, 0, data.Length)
+            Console.WriteLine("Sent: {0}", message)
+            let bufferArray : Byte array = Array.zeroCreate 256
+            let bytes = stream.Read(bufferArray, 0, bufferArray.Length)
+            let responseData = System.Text.Encoding.ASCII.GetString(bufferArray, 0, bytes)
+            Console.WriteLine("SERVER's RESPONSE {0}", responseData)
+
+        finally
+            printfn "Something Unexpected occured"
+    
+
+ClientSideProgram.connect ("","Hello from neww client")
---
+++ Server/Program.fs
@@ -0,0 +1,30 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System;
+open System.IO;
+open System.Net;
+open System.Net.Sockets;
+open System.Text;
+
+module ServerSideProgram=
+
+    let initiateServer() =
+        try
+            let port = 13000;
+            let ipAddress = IPAddress.Parse("127.0.0.1")
+            let server = TcpListener(ipAddress, port)
+            server.Start()
+            let data = null
+            while true do
+                Console.Write("Waiting for a connection... ")
+                let client = server.AcceptTcpClient()
+                Console.WriteLine("Connected")
+                let stream = client.GetStream()
+                let bufferArray : Byte array = Array.zeroCreate 256
+                let bytes = stream.Read(bufferArray, 0, bufferArray.Length)
+                let clientResponseData = System.Text.Encoding.ASCII.GetString(bufferArray, 0, bytes)
+                Console.WriteLine("Received From Client: {0}", clientResponseData)
+
+        finally
+            Console.WriteLine("Something Went Wrong")
+
+ServerSideProgram.initiateServer()
