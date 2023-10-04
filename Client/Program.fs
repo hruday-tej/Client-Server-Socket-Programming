4026c2f022b3893882846ad22cdc47127a9681ce
Emin Mugla <eminmugla09@gmail.com>
2023-10-04 00:48:30 -0400

    tested

--- Client/Program.fs
+++ Client/Program.fs
@@ -5,35 +5,55 @@ open System.Net.Sockets
 open System.Threading
 exception BreakException
 
-module ClientSideProgram=
+module ClientSideProgram =
     open System.Threading
     open System.Text
     let ipAddress = IPAddress.Parse("127.0.0.1")
     let serverAddress = "127.0.0.1"
     let port = 13000
     let cancellationTokenSource = new CancellationTokenSource()
+    let mutable continueProcessing = true
 
     let checkConnection(currentClient : TcpClient) =
-        while true do
-            try
-                let client = new TcpClient()
-                client.Connect(serverAddress, port)
-                let stream = client.GetStream()
-                let sendData = Encoding.ASCII.GetBytes("Heartbeat")
-                stream.Write(sendData, 0, sendData.Length)
-                client.Close()
-            with
-            | :? SocketException -> 
-                cancellationTokenSource.Cancel()
-                printf "SERVER IS DISCONNECTED"
-                currentClient.Close()
-                Environment.Exit(0)
-            Thread.Sleep(2000)
-    let connect () = 
         try
-            let port = 13000;
-            let tcpClient = new TcpClient("127.0.0.1", port)
-            while true do
+            let stream = currentClient.GetStream()
+            let sendData = Encoding.ASCII.GetBytes("heartbeat")
+            stream.Write(sendData, 0, sendData.Length)
+            try
+                // Try reading a response from the server
+                let bufferArray : byte[] = Array.zeroCreate 256
+                let bytes = stream.Read(bufferArray, 0, bufferArray.Length)
+                let responseData = System.Text.Encoding.ASCII.GetString(bufferArray, 0, bytes)
+                let test="0"
+                if responseData = "-0" then
+                     printfn "alive"
+                else
+                    printfn "SOMETHING FAILED"
+
+
+
+            with
+            | Failure(msg: string) -> printfn "SOMETHING FAILED"
+            | BreakException -> Console.WriteLine("Server Disconnected")
+        with
+        | Failure(msg: string) -> printfn "SOMETHING FAILED"
+        | BreakException -> Console.WriteLine("Server Disconnected")
+
+    let connect () =
+        try
+            let port = 13000
+            let tcpClient = new TcpClient(serverAddress, port)
+
+            // Start a separate thread to check server connection
+            let checkConnectionThread =
+                async {
+                    while continueProcessing do
+                        checkConnection tcpClient
+                        do! Async.Sleep(2000)
+                }
+            Async.Start(checkConnectionThread)
+
+            while continueProcessing do
                 Console.Write("Enter a command (e.g., 'add 5 8'): ")
                 let message = Console.ReadLine()
                 let data : byte[] = System.Text.Encoding.ASCII.GetBytes(message)
@@ -43,12 +63,14 @@ module ClientSideProgram=
                 let bytes = stream.Read(bufferArray, 0, bufferArray.Length)
                 let responseData = System.Text.Encoding.ASCII.GetString(bufferArray, 0, bytes)
                 Console.WriteLine("SERVER's RESPONSE {0}", responseData)
-
                 if responseData = "-5" then
-                        tcpClient.Close()
-                        raise BreakException
+                    Console.WriteLine("Received termination command. Exiting.")
+                    continueProcessing <- false
+                    tcpClient.Close()
+
         with
-        | Failure(msg: string) -> printfn "SOMETHING FAILED";
+        | Failure(msg: string) -> printfn "SOMETHING FAILED"
         | BreakException -> Console.WriteLine("Client Disconnected from Server")
 
 ClientSideProgram.connect()
+ 
--- Server/Program.fs
+++ Server/Program.fs
@@ -19,14 +19,13 @@ module ServerSideProgram=
         let mutable code = "pass"
         try
             if(array.Length = 1 && array.[0]= "bye") then code <- "-5"
-            elif(array.Length = 1 && array.[0]= "terminate") then code <- "-6"
+            elif(array.Length = 1 && array.[0]= "terminate") then code <- "-5" // Respond with -5 for both bye and terminate
+            elif(array.Length = 1 && array.[0]= "heartbeat") then code <- "-0" // Respond with -5 for both bye and terminate
             elif array.[0] <> "add" && array.[0] <> "subtract" && array.[0] <> "multiply" then raise(Error_Incorrect_Operation_Command())
             elif array.Length < 3 then raise(Error_Inputs_Less_Than_Two())
             elif array.Length > 5 then raise(Error_Inputs_More_Than_Four())
             for i=1 to array.Length-1 do int(array.[i]) |> ignore
             code
-
-
         with
             | :? Error_Incorrect_Operation_Command -> "-1"
             | :? Error_Inputs_Less_Than_Two -> "-2"
@@ -52,46 +51,50 @@ module ServerSideProgram=
                 result <- int(array.[1])
                 for i = 2 to array.Length-1 do
                     result <- result * int(array.[i])
-            elif (array.[0] = "bye")
-            then
-                result <- -5
-            elif (array.[0] = "terminate")
-            then
-                result <- -5
             code <- string(result)
         code
 
-    let rec clientCommunication (client: TcpClient, clientNum: int, server: TcpListener) =
+    let rec clientCommunication (client: TcpClient, clientNum: int) =
         try
             let stream = client.GetStream()
             let bufferArray : byte[] = Array.zeroCreate 256
-            let mutable continueProcessing = true
+            let  continueProcessing = true
 
             let handleRequest () =
                 let bytes = stream.Read(bufferArray, 0, bufferArray.Length)
                 if bytes = 0 then
                     // The client has disconnected, so stop processing
                     Console.WriteLine("Client {0} has disconnected.", clientNum)
-                    continueProcessing <- false
+                    continueProcessing = false
                 else
                     let clientRequestData = System.Text.Encoding.ASCII.GetString(bufferArray, 0, bytes)
-                    Console.WriteLine("Received From Client {0}: {1}", clientNum, clientRequestData)
                     let wordArray = clientRequestData.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)
-                    let serverResponseData = operate (wordArray)
-                    let wordArray = clientRequestData.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)
-                    let serverResponseData = operate (wordArray)
-                    let msg = System.Text.Encoding.ASCII.GetBytes(serverResponseData)
-                    stream.Write(msg, 0, msg.Length)
-                    Console.WriteLine("Responding to Client {0} with result: {1}", clientNum, serverResponseData)
-                    // Console.WriteLine("Response Sent to Client {0}", clientNum)
 
-                    let parts = clientRequestData.Split(' ')
-                    match parts with
-                    | [| "terminate" |] ->
-                        // cancellationTokenSource.Cancel()
-                        server.Stop()
-                    | _ ->
-                        printf ""
+                    // Check for termination commands
+                    if wordArray.Length = 1 && (wordArray.[0] = "bye" || wordArray.[0] = "terminate") then
+                        Console.WriteLine("Received From Client {0}: {1}", clientNum, clientRequestData)
+                        let terminationResponse = "-5"  // Both #bye and #terminate respond with -5
+                        let msg = System.Text.Encoding.ASCII.GetBytes(terminationResponse)
+                        stream.Write(msg, 0, msg.Length)
+                        Console.WriteLine("Responding to Client {0} with result: {1}", clientNum, terminationResponse)
+                        continueProcessing = false
+                        // If #terminate is received, set the serverRunning flag to false
+                        if wordArray.[0] = "terminate" then
+                            Console.WriteLine("Server Shutting Down")
+                            exit 0
+                    else if wordArray.Length = 1 && (wordArray.[0] = "heartbeat" ) then
+                        let responseAlive = "-0"  // Respond with -0 for heartbeat
+                        
+                        Console.WriteLine("Responding to Client with result: {0}", responseAlive)
+                        let msg = System.Text.Encoding.ASCII.GetBytes(responseAlive)
+                 
+                        stream.Write(msg, 0, msg.Length)
+                    else
+                        Console.WriteLine("Received From Client {0}: {1}", clientNum, clientRequestData)
+                        let serverResponseData = operate(wordArray)
+                        let msg = System.Text.Encoding.ASCII.GetBytes(serverResponseData)
+                        stream.Write(msg, 0, msg.Length)
+                        Console.WriteLine("Responding to Client {0} with result: {1}", clientNum, serverResponseData)
 
             while continueProcessing do
                 handleRequest()
@@ -100,7 +103,6 @@ module ServerSideProgram=
             | ex ->
                 Console.WriteLine("An error occurred with Client {0}: {1}", clientNum, ex.Message)
                 // Handle the error and continue processing other clients if needed
-
 
     let initiateServer() =
         try
@@ -114,13 +116,13 @@ module ServerSideProgram=
                 Console.Write("Waiting for a connection... ")
                 let client = server.AcceptTcpClient()
                 clientNum <- clientNum + 1
-                Console.WriteLine("Client {0} Connected",clientNum)
+                Console.WriteLine("Client {0} Connected", clientNum)
                 async {
                     do! Async.SwitchToThreadPool()
-                    clientCommunication(client, clientNum, server)
+                    clientCommunication(client, clientNum)
                 } |> Async.Start
         with
-            | Failure(msg) -> printfn "%s" msg
-            | ex -> printfn "An error occurred: %s" ex.Message
+        | Failure(msg) -> printfn "%s" msg
+        | ex -> printfn "An error occurred: %s" ex.Message
 
 ServerSideProgram.initiateServer()
